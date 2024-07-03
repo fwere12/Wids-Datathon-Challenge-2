e555052c98dbe137c9ca61deb3edede89acaa498
Faith Were <103557724+fwere12@users.noreply.github.com>
2024-07-03 23:07:00 +0300

    Data Preprocessing
    This is basic understanding of the two datasets

--- Libraries.py
+++ Libraries.py
@@ -8,3 +8,18 @@ test_data = pd.read_csv("C:/Users/Administrator/Downloads/widsdatathon2024-chall
 train_data = pd.read_csv("C:/Users/Administrator/Downloads/widsdatathon2024-challenge2/train.csv")
 test_data.head(5)
 train_data.head(5)
+
+#Chhecking for our data shape
+test_shape = test_data.shape
+print(test_shape)
+
+train_shape = train_data.shape
+print(train_shape)
+
+#Checking for missing values in our datasets
+test_data.isnull().sum()
+train_data.isnull().sum()
+
+#Summary statistics
+test_data.describe().T
+train_data.describe().T
